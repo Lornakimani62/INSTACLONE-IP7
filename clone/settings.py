5edf45770b24324eb8bec5f70a229367a636d7a6
lorna nashipae <kimanilorna@yahoo.com>
2018-12-16 02:58:31 +0300

    Adds a new root folder

--- clone/settings.py
+++ clone/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mjgzobqn4is2a8rc*pjv0(9y++brfy#r)6+uf=j&sy*tn=a)!d'
+SECRET_KEY = '+xry=9k%@ptj+ui7w5p^@r@!fwap=%54poy1k$p#r=1g0(@jd9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
