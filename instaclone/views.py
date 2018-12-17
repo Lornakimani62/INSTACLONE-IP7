a460efd612ce0c01271ec4b6dc3c36b8dbc04e7e
lorna nashipae <kimanilorna@yahoo.com>
2018-12-17 11:11:50 +0300

    Adds the login and sign up functionality

--- instaclone/views.py
+++ instaclone/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
+def index(request):
+    context = {}
+    return render (request, 'index.html' , context)
+
+def signup(request):
+    context = {}
+    return render (request, 'signup.html' , context)
